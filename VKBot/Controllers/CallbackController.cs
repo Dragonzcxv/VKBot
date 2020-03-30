b4a621bfd7aee9ea578192d11d084d8ed92b2a25
it003 <it003@eltrans.local>
2020-03-30 08:38:38 +0300

    Add project

---
+++ VKBot/Controllers/CallbackController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using VkNet.Abstractions;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
+using VkNet.Utils;
+
+namespace VKBot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CallbackController:ControllerBase
+    {
+        ///<summary>
+        ///Конфигурация приложения
+        ///</summary>
+        private readonly IConfiguration _configuration;
+
+        private readonly IVkApi _vkApi;
+        
+        public CallbackController(IVkApi vkApi,IConfiguration configuration)
+        {
+            _vkApi = vkApi;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public IActionResult Callback([FromBody]Updates updates)
+        {
+            switch (updates.Type)
+            {
+                case "confirmation":
+                    return Ok(_configuration["Config:Confirmation"]);
+                case "message_new":
+                    {
+                        // Десериализация
+                        var msg = Message.FromJson(new VkResponse(updates.Object));
+
+                        if(msg.Text.ToLower() == "да")
+                        {
+                            _vkApi.Messages.Send(new MessagesSendParams
+                            {
+                                RandomId = new DateTime().Millisecond,
+                                PeerId = msg.PeerId.Value,
+                                Message = "Ну тогда полезай на ножик"
+                            });
+                        }
+                        break;
+                    }
+            }
+            return Ok("ok");
+        }
+    }
+}
