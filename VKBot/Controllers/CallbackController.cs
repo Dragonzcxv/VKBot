dccb3669ed4818b0eee7a3f821c640854d0cafcb
Сергей Хохлов <Dragonzcxv@gmail.com>
2020-03-31 09:45:57 +0300

    п

--- VKBot/Controllers/CallbackController.cs
+++ VKBot/Controllers/CallbackController.cs
@@ -7,6 +7,12 @@ using VkNet.Model;
 using VkNet.Model.RequestParams;
 using VkNet.Utils;
 using System.Threading;
+using System.Text;
+using System.Net;
+using System.Collections.Generic;
+using VkNet.Model.Attachments;
+using VkNet.Model.Keyboard;
+using System.Collections;
 
 namespace VKBot.Controllers
 {
@@ -38,17 +44,33 @@ namespace VKBot.Controllers
                     {
                         // Десериализаци
                         var msg = Message.FromJson(new VkResponse(updates.Object));
-
-                        if(msg.Text.ToLower() == "да")
+                        var uploadServer = _vkApi.Docs.GetMessagesUploadServer(msg.PeerId.Value, DocMessageType.AudioMessage);
+                        var wc = new WebClient();
+                        var doc = Encoding.UTF8.GetString(wc.UploadFile(uploadServer.UploadUrl, @"D:\Downloads\Low Roar\2014 – Hávallagata 30 – EP\2.mp3"));
+                        //var voiceMes = _vkApi.Docs.Save(doc, "mes", "test")[0].Instance;
+                        var attac = new List<MediaAttachment>
                         {
-                            _vkApi.Messages.SetActivity("193439141", MessageActivityType.Typing, msg.PeerId.Value);
+                            _vkApi.Docs.Save(doc, "mes", "test")[0].Instance
+                        };
+                        
+
+                        if (msg.Text.ToLower() == "да")
+                        {
                             
+                            _vkApi.Messages.SetActivity("193439141", MessageActivityType.Typing, msg.PeerId.Value);
+                            Thread.Sleep(5000);
+                            KeyboardBuilder keyBuilder = new KeyboardBuilder();
+                            keyBuilder.AddButton("text", "Доп инфа", KeyboardButtonColor.Primary, "text");
+                            MessageKeyboard keyboard = keyBuilder.Build();
+                            
+
 
                             _vkApi.Messages.Send(new MessagesSendParams
                             {
                                 RandomId = new DateTime().Millisecond,
-                                PeerId = msg.PeerId.Value,
-                                Message = updates.Object.ToString()
+                                Keyboard = keyboard,
+                                PeerId = msg.PeerId.Value,             
+                                Message = updates.Type
                             });
                         }
                         break;
@@ -56,5 +78,8 @@ namespace VKBot.Controllers
             }
             return Ok("ok");
         }
+
+        
+        
     }
 }
